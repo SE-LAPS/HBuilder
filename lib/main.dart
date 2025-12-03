73e2bd27af358092873a3c6eb65cfafff060f7a8
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2025-12-03 23:57:22 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'firebase_options.dart';
 import 'config/theme.dart';
@@ -8,14 +9,11 @@ import 'providers/auth_provider.dart';
 import 'providers/location_provider.dart';
 
 import 'screens/auth/sign_in_screen.dart';
-import 'screens/main/main_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
@@ -29,26 +27,33 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => AuthProvider()),
         ChangeNotifierProvider(create: (_) => LocationProvider()),
       ],
-      child: MaterialApp(
-        title: 'HBuilder',
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme.lightTheme,
-        home: const SignInScreen(),
+      child: ScreenUtilInit(
+        designSize: const Size(375, 812), // Base design size (iPhone X)
+        minTextAdapt: true,
+        splitScreenMode: true,
         builder: (context, child) {
-          return Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Color(0xFFFF6B00), // Orange
-                  Color(0xFFFFD700), // Dark Yellow (Gold)
-                  Colors.white,      // White
-                ],
-                stops: [0.0, 0.5, 1.0],
-              ),
-            ),
-            child: child,
+          return MaterialApp(
+            title: 'Washtron',
+            debugShowCheckedModeBanner: false,
+            theme: AppTheme.lightTheme,
+            home: const SignInScreen(),
+            builder: (context, widget) {
+              return Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0xFFFF6B00), // Orange
+                      Color(0xFFFFD700), // Dark Yellow (Gold)
+                      Colors.white, // White
+                    ],
+                    stops: [0.0, 0.5, 1.0],
+                  ),
+                ),
+                child: widget,
+              );
+            },
           );
         },
       ),
